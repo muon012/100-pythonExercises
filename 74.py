1f69184ba819b1a9d3880530aa349ae677dc1254
muon012 <eduardessc0@hotmail.com>
2019-03-23 12:22:13 -0700

    new

--- 74.py
+++ 74.py
@@ -19,6 +19,7 @@
 # Solution
 import pandas
 
+
 data1 = pandas.read_csv("http://www.pythonhow.com/data/sampledata.txt")
 data2 = pandas.read_csv("http://pythonhow.com/data/sampledata_x_2.txt")
 new_data = pandas.concat([data1, data2]) # Must pass an iterable object
